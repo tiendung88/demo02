59cb785669c0657284acd3853138987eae2c2fd5
tiendung88 <tiendung88@gmail.com>
2016-06-28 06:46:28 +0700

    done gulp js

--- gulpfile.js
+++ gulpfile.js
@@ -2,28 +2,34 @@ var gulp = require('gulp');
 var sass = require('gulp-sass');
 var browserSync = require('browser-sync').create();
 
-
 gulp.task('sass', function () {
-  return gulp.src('app/scss/**/*.scss')
-    .pipe(sass.sync().on('error', sass.logError))
+  return gulp.src(['app/scss/*.scss', 'app/bower_components/bootstrap-sass/assets/stylesheets/bootstrap.scss'])
+    .pipe(sass().on('error', sass.logError))
     .pipe(gulp.dest('app/css'));
-});
-
-gulp.task('sass:watch', function () {
-  gulp.watch('app/scss/**/*.scss', ['sass']);
 });
 
 gulp.task('browserSync', function() {
   browserSync.init({
     server: {
-      baseDir: 'app'
+      baseDir: 'app/'
     },
   })
 })
-gulp.task('watch', ['browserSync', 'sass', 'sass:watch'], function (){
-  gulp.watch('app/scss/**/*.scss', ['sass']); 
+
+// Move file to .dist
+// gulp.task('fonts', function() {
+//   return gulp.src('app/fonts/**/*')
+//   .pipe(gulp.dest('dist/fonts'))
+// })
+
+gulp.task('watch', ['browserSync', 'sass'], function (){
+  gulp.watch('app/scss/*.scss', ['sass']); 
+  gulp.watch('app/bower_components/bootstrap-sass/assets/stylesheets/**/*.scss', ['sass']);
   // Reloads the browser whenever HTML or JS files change
   gulp.watch('app/*.html', browserSync.reload); 
   gulp.watch('app/css/*.css', browserSync.reload); 
+  gulp.watch('app/js/**/*.js', browserSync.reload); 
 });
 
+
+gulp.task('default', ['watch']);
